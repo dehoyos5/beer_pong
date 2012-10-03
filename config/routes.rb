ca7f86e329c68ec657b58483145d66b0c54af743
Christian DeHoyos <christian@uservoice.com>
2012-10-02 19:11:09 -0700

    updated index and admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 BeerPong::Application.routes.draw do
+  get "admin/admin"
+
   get "sessions/new"
 
   get "sessions/create"
@@ -11,6 +13,7 @@ BeerPong::Application.routes.draw do
 
   # Sample of regular route:
      match 'prices' => 'index#prices'
+     match  'admin' => 'admin#admin'
 
 
   get   '/login', :to => 'sessions#new', :as => :login
@@ -61,11 +64,11 @@ BeerPong::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'index#index'
+  # root :to => 'index#index'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  match ':controller(/:action(/:id))(.:format)'
 end
